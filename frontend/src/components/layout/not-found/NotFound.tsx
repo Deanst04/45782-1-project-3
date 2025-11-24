c1d317bae79ce0080812a84706290f8a90823a12
Dean Stark <deanstark04@gmail.com>
2025-11-24 12:25:44 +0200

    css complete

--- frontend/src/components/layout/not-found/NotFound.tsx
+++ frontend/src/components/layout/not-found/NotFound.tsx
@@ -1,9 +1,27 @@
+import { useNavigate } from 'react-router-dom'
+import useTitle from '../../../hooks/use-title'
 import './NotFound.css'
+import useRole from '../../../hooks/use-role'
 
 export default function NotFound() {
+
+    useTitle('404')
+    const role = useRole()
+    const navigate = useNavigate()
+
+    const target = role === 'admin' ? '/admin' : '/user'
+    const label = role === 'admin' ? 'back to admin page' : 'back to vacations'
+
     return (
         <div className='NotFound'>
-            404 not found, where is your head?
+            <div className='nf-plane'>✈️ You navigated like a Ryanair pilot</div>
+            <div className='notfound-content'>
+                <h1 className='nf-title'>404</h1>
+                <p className='nf-sub'>not found, where is your head?</p>
+                <button onClick={() => navigate(`${target}`)} className='nf-btn'>
+                    {label}
+                </button>
+            </div>
         </div>
     )
 }
